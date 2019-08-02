113e170e29705a8f1f47e302d8e7d3befa8f6461
dennystasaski <dstasaski@gmail.com>
2019-08-02 16:45:59 -0500

    initial docker compose working

--- redditstream/app.py
+++ redditstream/app.py
@@ -2,9 +2,22 @@ import praw
 import os
 import json
 
-from kafka import KafkaProducer
+from time import sleep
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
+from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
+
+
+producer = None
+
+while True:
+    try:
+        producer = KafkaProducer(bootstrap_servers=['kafka:9092'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
+        break
+    except NoBrokersAvailable:
+        print("Waiting for Kafka Broker...")
+        sleep(5)
+
 
 reddit = praw.Reddit(client_id=os.environ['client_id'],
                      client_secret=os.environ['client_secret'],
@@ -13,6 +26,7 @@ reddit = praw.Reddit(client_id=os.environ['client_id'],
 subreddit = reddit.subreddit('all')
 for comment in subreddit.stream.comments(skip_existing=True):
     producer.send('reddit', {'body': comment.body})
+    print("we made it!")
 
 
 
